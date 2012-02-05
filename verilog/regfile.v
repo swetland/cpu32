d391047f775ac30eedaa55121306be9135862bfd
Brian Swetland <swetland@frotz.net>
2012-02-05 10:35:51 -0800

    regfile: don't initialize

--- verilog/regfile.v
+++ verilog/regfile.v
@@ -11,17 +11,6 @@ module regfile #(parameter DWIDTH=16, parameter AWIDTH=3) (
 
 reg [DWIDTH-1:0] R[0:2**AWIDTH-1];
 
-initial begin
-	R[0] <= 0;
-	R[1] <= 0;
-	R[2] <= 0;
-	R[3] <= 0;
-	R[4] <= 0;
-	R[5] <= 0;
-	R[6] <= 0;
-	R[7] <= 0;
- end
-
 always @ (posedge clk)
 	if (we)
 		R[wsel] <= wdata;
